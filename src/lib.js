7a287dacd88282318cb7c3c994414071dead46f5
Reshmi R S <44020004+reshmisaji@users.noreply.github.com>
2018-10-25 16:22:41 +0530

    add function to make cycler that can cycle three elements passed even one element added later

--- src/lib.js
+++ src/lib.js
@@ -81,7 +81,7 @@ const makeCycler = function(collection){
   let length = collection.length;
   if(length!=collection.length){index=0;}
  const toggle = function(){ 
-   if(index>=collection.length){index = 0;}
+   if(index>=length){index = 0;}
   let returnValue = collection[index];
    index++;
    return returnValue;
